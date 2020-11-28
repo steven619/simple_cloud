0b498119e644933b0741c7edbd763924c85016dc
steven619 <yangxu185333467@gmail.com>
2020-11-28 15:22:12 +0800

    加了原生的排序代码.一般这种需要标出来的数据.都是再后端弄好,传给前端的.所以感觉写的不是很好

---
+++ setOnKeyListener.java
@@ -0,0 +1,50 @@
+
+    public static int[] bubbleSort(int[] array) {
+        if (array.length == 0)
+            return array;
+        for (int i = 0; i < array.length; i++)
+            for (int j = 0; j < array.length - 1 - i; j++)
+                if (array[j + 1] < array[j]) {
+                    int temp = array[j + 1];
+                    array[j + 1] = array[j];
+                    array[j] = temp;
+                }
+        return array;
+    }
+
+
+   public static int[] selectionSort(int[] array) {
+        if (array.length == 0)
+            return array;
+        for (int i = 0; i < array.length; i++) {
+            int minIndex = i;
+            for (int j = i; j < array.length; j++) {
+                if (array[j] < array[minIndex]) //找到最小的数
+                    minIndex = j; //将最小数的索引保存
+            }
+            int temp = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = temp;
+        }
+        return array;
+    }
+
+private ProgressDialog progressDialog;
+
+ProgressDialog show(Context context, CharSequence title, CharSequence message, boolean indeterminate, boolean cancelable) 
+
+public void showProgressDialog(String title, String message) {
+        if (progressDialog == null) {
+            progressDialog = ProgressDialog.show(UploadActivity.this, title,message, false, false);
+        } else if (progressDialog.isShowing()) {
+            progressDialog.setTitle(title);
+            progressDialog.setMessage(message);
+        }
+        progressDialog.show();
+    }
+
+public void hideProgressDialog() {
+        if (progressDialog != null && progressDialog.isShowing()) {
+            progressDialog.dismiss();
+        }
+    }
